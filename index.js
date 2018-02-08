b089a025731f2d48abc2087204156e374780d110
Tien Nguyen <pri859@sudomail.com>
2018-02-08 00:00:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function HappyHolidays() {
   return "Happy Holidays!"
 }
+
 function happyHolidaysTo(name) {
     return `Happy holidays, ${name}!`
  }
+ 
 function holidayCountdown() {
   return '`It's ${days} days until ${holiday}!`
 }
+
